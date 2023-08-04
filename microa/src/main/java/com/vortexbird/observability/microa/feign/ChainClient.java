d2571d75f4ff5949dcdb72981731b9e767a9ef66
Johan Steve Bejarano Fiesco <jbejarano@vortexbird.com>
2023-08-04 17:01:05 -0500

    Cambio en la invocación directa

--- microa/src/main/java/com/vortexbird/observability/microa/feign/ChainClient.java
+++ microa/src/main/java/com/vortexbird/observability/microa/feign/ChainClient.java
@@ -4,7 +4,7 @@ import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
-@FeignClient(url = "http://localhost:8429/microb/api/v1", value = "chain-service")
+@FeignClient(url = "http://localhost:9534/api/v1", value = "chain-service")
 public interface ChainClient {
     
     @GetMapping(path = "/chain/{name}") 
--- microb/src/main/java/com/vortexbird/observability/microb/feign/ChainClient.java
+++ microb/src/main/java/com/vortexbird/observability/microb/feign/ChainClient.java
@@ -4,7 +4,7 @@ import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
-@FeignClient(url = "http://localhost:8429/microc/api/v1", value = "chain-service")
+@FeignClient(url = "http://localhost:9535/api/v1", value = "chain-service")
 public interface ChainClient {
     
     @GetMapping(path = "/chain/{name}") 
